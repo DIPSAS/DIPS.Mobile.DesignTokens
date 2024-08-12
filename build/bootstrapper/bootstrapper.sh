48865cd264017d0f3c204439ae78a33c30738735
Sander Nicolausson <sander-nico@hotmail.com>
2024-08-12 10:15:08 +0200

    silent install style-dictionary

--- build/bootstrapper/bootstrapper.sh
+++ build/bootstrapper/bootstrapper.sh
@@ -8,15 +8,13 @@ if dotnet tool list -g | grep dotnet-script > /dev/null ; then
    echo "✅ dotnet-script was found"
 else
    echo "❌ dotnet-script was not found, installing..."
-   npm_config_loglevel=silent
    dotnet tool install -g dotnet-script > /dev/null
-   npm_config_loglevel=error
 fi
 
 if npm list --prefix ./src | grep style-dictionary > /dev/null ; then
    echo "✅ npm package: style-dictionary was found"
    echo "style-dictionary version: $(npm view style-dictionary version)"
 else
-   npm install --prefix ./src style-dictionary
+   npm install --prefix ./src style-dictionary --silent
    echo "style-dictionary version: $(npm view style-dictionary version)"
 fi
